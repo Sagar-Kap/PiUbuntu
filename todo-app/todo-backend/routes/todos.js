8a1af2a846239e77e1a2fc340f1f8a4e15ab5e6d
Sagar Kapoor <sagrkapr@gmail.com>
2023-03-23 15:51:33 +0200

    ✅ Completed exercise 12.10

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { Todo } = require("../mongo");
 const router = express.Router();
+const { getAsync, setAsync } = require("../mongo");
 
 /* GET todos listing. */
 router.get("/", async (_, res) => {
@@ -14,6 +15,9 @@ router.post("/", async (req, res) => {
     text: req.body.text,
     done: false,
   });
+  const current = await getAsync("todocounter");
+  const newValue = parseInt(current) + 1;
+  await setAsync("todocounter", newValue.toString());
   res.send(todo);
 });
 
